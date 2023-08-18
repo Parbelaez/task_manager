6c91402a2a9014baf124761b9a35f523bf88223a
Parbelaez <132989779+Parbelaez@users.noreply.github.com>
2023-08-18 22:18:26 +0200

    Create the task page and form

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,15 @@ document.addEventListener('DOMContentLoaded', function () {
     // Initialize sidenav
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: 'yyyy-mm-dd',
+      i18n: { done: 'Select' }
+    });
+
+    // select initialization
+    let select = document.querySelectorAll('select');
+    M.FormSelect.init(select);
   });
